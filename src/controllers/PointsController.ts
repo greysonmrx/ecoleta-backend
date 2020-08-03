c753ddb1663db764fee0aba7b8bc727c75437a4e
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-03 15:34:45 -0300

    feat: create route to list all points

--- src/controllers/PointsController.ts
+++ src/controllers/PointsController.ts
@@ -3,6 +3,31 @@ import { Request, Response } from 'express';
 import knex from '../database/connection';
 
 class PointsController {
+  async index(request: Request, response: Response): Promise<Response> {
+    const { city, uf, items } = request.query;
+
+    const parsedItems = String(items)
+      .split(',')
+      .map(item => Number(item.trim()));
+
+    const points = await knex('points')
+      .join('point_items', 'points.id', '=', 'point_items.point_id')
+      .whereIn('point_items.item_id', parsedItems)
+      .where('city', String(city))
+      .where('uf', String(uf))
+      .distinct()
+      .select('points.*');
+
+    const serializedPoints = points.map(point => {
+      return {
+        ...point,
+        image_url: `http://192.168.100.52:5000/uploads/${point.image}`,
+      };
+    });
+
+    return response.json(serializedPoints);
+  }
+
   async store(request: Request, response: Response): Promise<Response> {
     const {
       name,
