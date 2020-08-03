e438121ae9263484e69f964b3d72ca23daa3a7d5
Greyson Mascarenhas <greysonmrx@gmail.com>
2020-08-03 13:21:49 -0300

    chore: setup for seeds to populate items table

---
+++ src/database/seeds/00_create_items.ts
@@ -0,0 +1,30 @@
+import Knex from 'knex';
+
+export default async function seed(knex: Knex): Promise<void> {
+  await knex('items').insert([
+    {
+      title: 'Lâmpadas',
+      image: 'lampadas.svg',
+    },
+    {
+      title: 'Pilhas e baterias',
+      image: 'baterias.svg',
+    },
+    {
+      title: 'Papéis e Papelão',
+      image: 'papeis-papelao.svg',
+    },
+    {
+      title: 'Resíduos Eletrônicos',
+      image: 'eletronicos.svg',
+    },
+    {
+      title: 'Resíduos Orgânicos',
+      image: 'organicos.svg',
+    },
+    {
+      title: 'Óleo de Cozinha',
+      image: 'oleo.svg',
+    },
+  ]);
+}
